5d1adad520f3e7d5ebbd4330d82c9f225062bf86
grez-lucas <lucasgrez3010@gmail.com>
2023-12-18 19:41:22 -0300

    feat: home_page and initial navigation widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/app_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('History'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {}),
+        ],
+      ),
       body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+        child: Text('Home Page'),
+      ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
