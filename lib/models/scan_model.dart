31f920c92bdd02ad75cc9cdde7eff119f3a341cd
grez-lucas <lucasgrez3010@gmail.com>
2023-12-19 13:12:40 -0300

    feat:  ScanModel class for JSON parsing

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,33 @@
+// To parse this JSON data, do
+//
+//     final scanModel = scanModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+    int id;
+    String type;
+    String value;
+
+    ScanModel({
+        required this.id,
+        required this.type,
+        required this.value,
+    });
+
+    factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+    };
+}
