7a726d03d222c8acd2b2cfc8bd12f590a4788779
grez-lucas <lucasgrez3010@gmail.com>
2023-12-19 12:53:31 -0300

    feat: Barcode being initialized

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -6,7 +7,20 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancel',
+          false,
+          ScanMode.BARCODE,
+        );
+
+        if (barcodeScanRes == '-1') {
+          return;
+        }
+
+        print('The barcode is: $barcodeScanRes');
+      },
       elevation: 0,
       backgroundColor: Theme.of(context).primaryColor,
       child: const Icon(Icons.filter_center_focus),
