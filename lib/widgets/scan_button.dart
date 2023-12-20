3f553f2a924f4d00f72584126843e51798fed78d
grez-lucas <lucasgrez3010@gmail.com>
2023-12-20 12:56:36 -0300

    feat: update barcode scanning to use QR code mode

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -12,14 +12,14 @@ class ScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-        //   '#3D8BEF',
-        //   'Cancel',
-        //   false,
-        //   ScanMode.BARCODE,
-        // );
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancel',
+          false,
+          ScanMode.QR,
+        );
 
-        final barcodeScanRes = 'geo:-33.49,-70.66';
+        // final barcodeScanRes = 'geo:-33.49,-70.66';
         // final barcodeScanRes = 'https://www.facebook.com';
 
         if (barcodeScanRes == '-1') {
